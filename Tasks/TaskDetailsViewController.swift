516eebd8799c0098ab0f9953cd59e68f45be9a2a
Matthew Nielsen <mqnielsen@me.com>
2015-02-24 19:25:58 -0500

    Removed a bunch of unneeded comments.

--- Tasks/TaskDetailsViewController.swift
+++ Tasks/TaskDetailsViewController.swift
@@ -27,7 +27,6 @@ class TaskDetailsViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func createTask() {
@@ -59,15 +58,5 @@ class TaskDetailsViewController: UIViewController {
             navController.popViewControllerAnimated(true)
         }
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
 }
